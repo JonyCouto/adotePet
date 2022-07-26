c84c9f1059d714a57a79ab6b04ab1b00c19eaad3
Jonathan Couto <jonathancouto3030@hotmail.com>
2022-07-26 13:47:19 -0300

    Inserindo projeto adotePet

---
+++ js/funcoes.js
@@ -0,0 +1,16 @@
+function movimentaCarrosel(i, tamanhoSlide, slide){
+    i++
+    i > tamanhoSlide - 1 ? i = 0 : i = i
+    console.log(i)
+    slide.style.transform = `translateX(${-i*100}%)`
+    setTimeout(() => {
+        movimentaCarrosel(i, tamanhoSlide, slide)
+    }, 6000)
+}
+function carrosel(){
+    const slide = document.querySelector('.container')
+    const tamanhoSlide = Array.from(document.querySelectorAll('.divImgSlide')).length
+    const i = 0
+    movimentaCarrosel(i, tamanhoSlide, slide)
+}
+carrosel()
